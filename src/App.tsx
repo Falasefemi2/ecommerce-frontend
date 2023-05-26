fb8c2d398c964b340e94dda94922cda15ea5f61e
Falase Femi <femifalase228@gmail.com>
2023-05-26 08:51:43 +0100

    tailwind installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Inspx from "inspx"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <Inspx>
+      <div className=" text-3xl text-marian-blue">Hello WorldI</div>
+      <div className=" text-3xl text-red">Hello World</div>
+      <div className=" text-3xl text-sea-salt">Hello World</div>
+      <div className=" text-3xl text-aquamarine">Hello World</div>
+      <div className=" text-3xl text-marian-blue">Hello World</div>
+      </Inspx>
+    </div>
   )
 }
 
